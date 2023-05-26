d14d83a9902fb2c76e091170630f1e125cf73f01
Jose Eneas da Silva Maria <joseeneas@me.com>
2023-05-26 17:59:25 -0400

    Final commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e9c9877e91ae5eb5170adfe4993505cf"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2a1484bc88ca412989828c0d2fec1e3b"
